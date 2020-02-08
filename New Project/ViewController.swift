fab672c92efc184ebbab0c2f7ad5da087736bc5a
amishbanjara <52459243+amishbanjara@users.noreply.github.com>
2020-02-08 14:20:01 -0600

    Initial Commit

---
+++ New Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  New Project
+//
+//  Created by Amish Banjara on 2/8/20.
+//  Copyright © 2020 Amish Banjara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
